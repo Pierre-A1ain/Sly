2d646e4d90393b80a7b83d0bf42ee86481097302
Pierre-A1ain <p.martignoles@gmail.com>
2024-05-09 06:34:44 +0200

    Ajout du lien vers Bootstrap dans les pages web

--- Gestion_entreprise.php
+++ Gestion_entreprise.php
@@ -73,10 +73,7 @@ $entreprises = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ajouter et supprimer des entreprises</title>
-    <style>
-        label, input { display: block; }
-        input { padding: 5px; margin-bottom: 10px; }
-    </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
     <h1>Ajouter et supprimer des entreprises</h1>
--- Header.php
+++ Header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Fantasy V1</title>
 </head>
 <body>
@@ -16,7 +16,7 @@
     <nav class="navbar">
         <ul>
             <li><a href="LambingPage.php">Accueil</a></li>
-            <li><a href="Gestion_Entreprise.php">Gérer ses cheptels</a></li>
+            <li><a href="Gestion_entreprise.php">Gérer ses cheptels</a></li>
         </ul>
     </nav>
 
